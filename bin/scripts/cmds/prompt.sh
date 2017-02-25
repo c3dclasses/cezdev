e2e0fdcae62290fd426a349178799c64600fe80a
kevin <c3dclasses@gmail.com>
2017-02-25 18:16:14 -0500

    created the prompt.sh command

---
+++ bin/scripts/cmds/prompt.sh
@@ -0,0 +1,7 @@
+##------------------------------------------------------------------------------------------
+## name: prompt.sh
+## desc: shows prompt dialog to get input from user 
+## usage: prompt.sh <msg> <title> <warn> <choices.json>
+##------------------------------------------------------------------------------------------
+
+java_run.sh CInputBox "$@"
